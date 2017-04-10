5c81ba35639280446d597a864f3434c39110a6fa
Ramanan Rajeswaran <ramanan@google.com>
2017-04-10 14:10:46 -0700

    Add aosp bits.
    Test: built muskie.
    Change-Id: Idf5c79ea12b8ca67d747dde20173b2e58a413555

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+
 $(call inherit-product, device/google/muskie/device-walleye.mk)
 $(call inherit-product-if-exists, vendor/google_devices/muskie/device-vendor-walleye.mk)
 
