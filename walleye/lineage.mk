49faa6b7b66b46e56ae3da08b308dbacf0ba10cf
Abhisek Devkota <ciwrl@lineageos.org>
2017-12-23 22:10:18 -0800

    Initial Lineage mk files and dependencies
    Change-Id: I1dbc1fd122da9de7f691f09a472eade1057379c0

---
+++ walleye/lineage.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/muskie/aosp_walleye.mk)
+
+-include device/google/wahoo/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_walleye
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2
+TARGET_MANUFACTURER := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=walleye \
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+$(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
