c50eefb7bd047b25ec442bf3d3b190e8ab44fa28
Abhisek Devkota <ciwrl@lineageos.org>
2018-03-10 01:05:14 -0800

    Walleye: Update fingerprint to OPM1.171019.021
    Change-Id: Idb8acd61716ea0dd248431431a07b2d4f9e9f054

--- walleye/lineage.mk
+++ walleye/lineage.mk
@@ -18,7 +18,7 @@ TARGET_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.019/4527419:user/release-keys \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.019 4527419 release-keys"
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
