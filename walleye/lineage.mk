3fe0c5ca6087bc5ea07db88e685a6ec415c6d010
Abhisek Devkota <ciwrl@lineageos.org>
2018-04-04 09:46:51 -0700

    Swap to new Fingerprint override mechanism
    Change-Id: I1b8fecf79c08fbada13fde8b729dacafecdb2f04

--- walleye/lineage.mk
+++ walleye/lineage.mk
@@ -18,7 +18,8 @@ TARGET_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM2.171019.029/4657601:user/release-keys \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+
+BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM2.171019.029/4657601:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
