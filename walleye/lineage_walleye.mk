f26b5f096d3593ac7199f877b724c4cba3640934
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-12 14:10:25 -0700

    walleye: Update fingerprint from PPR2.181005.003
    Change-Id: I6123786a367b917a52533f9f3f460187745c4a34

--- walleye/lineage_walleye.mk
+++ walleye/lineage_walleye.mk
@@ -18,8 +18,8 @@ TARGET_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    PRIVATE_BUILD_DESC="walleye-user 9 PPR1.180610.009 4898911 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 9 PPR2.181005.003 4984323 release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:9/PPR1.180610.009/4898911:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:9/PPR2.181005.003/4984323:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
