0b163c0e5e1a9334d0cff1a60bb918f4b5493af6
Abhisek Devkota <ciwrl@lineageos.org>
2020-03-17 23:16:17 +1100

    walleye: Initial Lineage makefile and dependencies
    Change-Id: I1dbc1fd122da9de7f691f09a472eade1057379c0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_walleye.mk \
     $(LOCAL_DIR)/aosp_walleye_test.mk \
+    $(LOCAL_DIR)/walleye/lineage_walleye.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_walleye-userdebug \
