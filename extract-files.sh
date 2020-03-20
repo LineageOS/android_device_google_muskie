6c3763c6db38ab4f9a56c954e949ce36c39674fe
Eamon Powell <eamonpowell@outlook.com>
2020-03-20 21:08:00 +1100

    walleye: Add proprietary vendor files
    Change-Id: I15bbdef43217fd98cd4b33a3940bc273e1342b0c

--- extract-files.sh
+++ extract-files.sh
@@ -59,5 +59,6 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/lineage-proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/lineage-proprietary-vendor-files.txt "$SRC" "$SECTION"
 
 "$MY_DIR"/setup-makefiles.sh
