7d7b4b1c184926d525170f70a95af3bc3a0d55c5
Sal Savage <salsavage@google.com>
2022-04-15 15:38:17 -0700

    wahoo: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53

--- device-common.mk
+++ device-common.mk
@@ -28,6 +28,10 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.audio.adm.buffering.ms=3
 PRODUCT_PROPERTY_OVERRIDES += audio_hal.period_multiplier=2
 PRODUCT_PROPERTY_OVERRIDES += af.fast_track_multiplier=1
 
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.device.default_name=Pixel 2
+
 # Whether by default, the eSIM system UI, including that in SUW and Settings, will be shown.
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += esim.enable_esim_system_ui_by_default=false
 
