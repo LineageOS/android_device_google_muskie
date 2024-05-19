05d8abb834cb754d6bad69e20a0e64364736df6a
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    walleye: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -19,7 +19,3 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_walleye_test.mk \
     $(LOCAL_DIR)/aosp_walleye_hwasan.mk \
     $(LOCAL_DIR)/walleye/lineage_walleye.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosp_walleye-userdebug \
-    aosp_walleye_test-userdebug
