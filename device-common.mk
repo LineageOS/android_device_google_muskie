76018373a3304cd3b3f33e764b8710e3412510d9
Marc Bourgoin <themard85@gmail.com>
2024-11-21 13:24:16 -0700

    walleye: Remove copy libutils-v33
    With enforced elf checks coming on A16, let's switch to the provided one from compat
    
    Change-Id: I193682ca769e902b7a8b2edd850ea14297a71dec

--- device-common.mk
+++ device-common.mk
@@ -105,12 +105,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.notify_nfc=1
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
-
 PRODUCT_PACKAGES += \
     libhidlbase-v32 \
-    libhidlbase-v32.vendor
+    libhidlbase-v32.vendor \
+    libutils-v33
 
 # DRM
 PRODUCT_PACKAGES += \
